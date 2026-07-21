256446d6f74c29baa7d38822d043f3beec4401af
Loic <lsbillaut@gmail.com>
2026-07-20 20:28:02 -0700

    Moved configurable values into settings.py

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from image_manager import ImageManager
-from Slideshow import Slideshow
+from slideshow import Slideshow
 import pygame
+from settings import *
 from pathlib import Path
 
 pygame.init()
 
 running = True
-WIDTH = 720 # place in settings.py
-HEIGHT = 720
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-img_manager = ImageManager("images")
+img_manager = ImageManager(IMAGE_FOLDER)
 slideshow = Slideshow(screen)
 
 slideshow.display(img_manager.current_image())
@@ -22,7 +21,7 @@ while running:
     current_time = pygame.time.get_ticks()
     if current_time - start_time > 5000: 
         slideshow.display(img_manager.next_image())
-        start_time = pygame.time.get_ticks()
+        start_time = current_time
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
---
+++ settings.py
@@ -0,0 +1,12 @@
+# Window Settings
+WIDTH = 720
+HEIGHT = 720
+WINDOW_TITLE = "Picture Frame"
+BACKGROUND_COLOR = "black"
+
+# Slideshow settings
+DISPLAY_TIME_MS = 5000
+
+# Images
+IMAGE_FOLDER = "images"
+SUPPORTED_IMAGE_FILES = {".jpg", ".jpeg", ".png", ".bmp"}
